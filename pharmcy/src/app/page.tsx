3178e1fb3c62d7cb36c6fb29aa0b42fc6514bee8
mayadamansour1994@gmail.com <mayadamansour1994@gmail.com>
2026-05-24 17:02:29 +0300

    Create professional pharmacy homepage UI

--- pharmcy/src/app/page.tsx
+++ pharmcy/src/app/page.tsx
@@ -1,33 +1,20 @@
-import BannerHome from "../components/home/BannerHome";
 import CategoriesHome from "../components/home/CategoryHome";
 import Hero from "../components/home/Hero";
 import ProductHome from "../components/home/ProductHome";
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-hidden bg-[#f8fbff]">
-
-      {/* HERO */}
+    <main className="min-h-screen">
       <Hero />
 
-      {/* FLOATING BANNER */}
-      <section className="relative z-30 -mt-8 lg:-mt-14">
 
-        <div className="mx-auto max-w-[1200px] px-4">
 
-          <BannerHome />
-
-        </div>
-      </section>
-
-      {/* PRODUCTS */}
-      <div className="mx-auto px-4 space-y-12 py-10">
-
+      <div className=" mx-auto px-4 space-y-12 py-10">
         <CategoriesHome />
 
         <ProductHome />
-
       </div>
     </main>
   );
 }
+
